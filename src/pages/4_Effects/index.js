634ee4e23a8ef9b9d12841ac051d81221cdc783b
Gianfranco <gmeneguz@gmail.com>
2021-09-22 16:20:16 -0300

    update: use api call

--- src/pages/4_Effects/index.js
+++ src/pages/4_Effects/index.js
@@ -2,15 +2,32 @@ import React, { useEffect, useState } from "react";
 
 function Effects() {
   const [searchText, setSearchText] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     if (searchText && searchText.length > 3) {
-      alert("Realizando Busca");
+      setIsSearching(true);
+      const url = process.env.REACT_APP_API_URL + "/users?search=" + searchText;
+      console.log(`Buscando Usuários em: ${url}`);
+      fetch(url)
+        .then((res) => res.json())
+        .then((result) => {
+          setUsers(result);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Erro ao processar a requisição");
+        })
+        .finally(() => {
+          setIsSearching(false);
+        });
     }
   }, [searchText]);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", fontSize: "38px" }}>
+    <div style={{ display: "flex", flexDirection: "column", fontSize: "32px" }}>
+      <small>Buscar Usuários (digitar ao menos 4 letras)</small>
       <form>
         <input
           value={searchText}
@@ -18,6 +35,13 @@ function Effects() {
           placeholder="Controlled"
         />
       </form>
+      {isSearching && <div>processando...</div>}
+      <h3>Lista de Usuários</h3>
+      <ul>
+        {users.map((user) => (
+          <li>Nome: {user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
